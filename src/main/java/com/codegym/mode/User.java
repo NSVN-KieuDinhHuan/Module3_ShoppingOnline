ffd6f54b427b17f8f4233a001f80d14509639c32
hoale0507 <hoale090293@gmail.com>
2022-03-10 10:45:50 +0700

    change role_id datatype

--- src/main/java/com/codegym/mode/User.java
+++ src/main/java/com/codegym/mode/User.java
@@ -7,7 +7,7 @@ public class User {
     private  String address;
     private  String phone;
     private  String Password;
-    private String role_id;
+    private int role_id;
     private boolean status;
     public User() {
     }
